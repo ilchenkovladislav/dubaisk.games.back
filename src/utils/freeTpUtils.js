07d0750d034b0705db07d924af2b631bda5b5401
Ilchenko <ilchenkowd@gmail.com>
2025-07-08 13:49:06 +0300

    добавление функции для нахождения разницы в массивах

--- src/utils/freeTpUtils.js
+++ src/utils/freeTpUtils.js
@@ -3,3 +3,8 @@ export function extractFreetpGameTitle(input) {
   const match = input.match(regex)
   return match ? match[1].trim() : null
 }
+
+export function getDifferenceByTitle(arr1, arr2) {
+  const titles2 = new Set(arr2.map((obj) => obj.title))
+  return arr1.filter((obj) => !titles2.has(obj.title))
+}
