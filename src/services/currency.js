5f72a39e8200bd19dfa84c8efc5f856ee218ab4a
ilchenko <ilchenkowd@gmail.com>
2025-08-18 15:56:26 +0500

    изменение источника курса валют

--- src/services/currency.js
+++ src/services/currency.js
@@ -1,16 +1,7 @@
 import axios from 'axios'
 
 export async function getCurrency() {
-  const resKZT = await axios.get(
-    'https://steam-rates.playwallet.bot/latest-rate/?currency_code=KZT&base_currency_code=USD',
-  )
+  const rates = await axios.get('https://www.cbr-xml-daily.ru/latest.js')
 
-  const resRUB = await axios.get(
-    'https://steam-rates.playwallet.bot/latest-rate/?currency_code=RUB&base_currency_code=USD',
-  )
-
-  const kztRate = await resKZT.data[0].exchange_rate
-  const rubRate = await resRUB.data[0].exchange_rate
-
-  return rubRate / kztRate
+  return rates.data.rates['KZT']
 }
